ceb192d9befb59bdc5d9f85aa4d78c537f67fa2f
IrinaB <balkarovairina@gmail.com>
2024-02-19 07:34:22 +0100

    Add React Router with Links

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
-import { Product } from "../../app/models/product";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@mui/material'
+import { Product } from '../../app/models/product'
+import { Link } from 'react-router-dom'
 
 interface Props {
     product: Product;
@@ -16,7 +17,7 @@ export default function ProductCard({ product }: Props) {
                 }
                 title={product.name}
                 titleTypographyProps={{
-                    sx: { fontWeight: 'bold', color: 'primary.main' }
+                    sx: { fontWeight: 'bold', color: 'primary.main' },
                 }}
             />
             <CardMedia
@@ -25,16 +26,16 @@ export default function ProductCard({ product }: Props) {
                 title={product.name}
             />
             <CardContent>
-                <Typography gutterBottom color='secondary' variant="h5" component="div">
+                <Typography gutterBottom color='secondary' variant='h5' component='div'>
                     ${(product.price / 100).toFixed(2)}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant='body2' color='text.secondary'>
                     {product.brand} / {product.type}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Add to Cart</Button>
-                <Button size="small">View</Button>
+                <Button size='small'>Add to Cart</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size='small'>View</Button>
             </CardActions>
         </Card>
     )
